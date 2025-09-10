3bf74a28002aa3f17cfdf264bcd96b18ef2f0146
Гребенюк Герман Станиславович <GGrebenyuk@alfabank.ru>
2025-09-11 00:12:05 +0300

    removed separate API tests

--- tests/api/test_complete_task.py
+++
@@ -1,7 +0,0 @@
-from qag_python_final_project_todoist.api_model.clients.tasks import TasksClient
-from http import HTTPStatus
-
-
-def test_complete_task(tasks_client: TasksClient):
-    got = tasks_client.complete_task()
-    assert got.status_code == 204
--- tests/api/test_delete_task.py
+++
@@ -1,7 +0,0 @@
-from qag_python_final_project_todoist.api_model.clients.tasks import TasksClient
-from http import HTTPStatus
-
-
-def test_delete_task(tasks_client: TasksClient):
-    got = tasks_client.delete_task()
-    assert got.status_code == 204
