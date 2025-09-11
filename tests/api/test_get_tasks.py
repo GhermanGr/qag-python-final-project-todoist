b79a63a527c16bca97d85304453f3788fb112b2d
Гребенюк Герман Станиславович <GGrebenyuk@alfabank.ru>
2025-09-11 23:12:36 +0300

    returned separate API tests

---
+++ tests/api/test_get_tasks.py
@@ -0,0 +1,20 @@
+# from jsonschema import validate
+# from qag_python_final_project_todoist.api_model.schemas.get_tasks_schema import (
+#     GET_TASKS_SCHEMA,
+# )
+# from qag_python_final_project_todoist.api_model.clients.get_tasks import get_tasks
+
+
+# def test_get_tasks(setup_api_requests):
+#     response, body = get_tasks()
+#     assert response.status_code == 200
+
+#     validate(body, GET_TASKS_SCHEMA)
+
+from qag_python_final_project_todoist.api_model.clients.tasks import TasksClient
+from http import HTTPStatus
+
+
+def test_get_tasks(tasks_client: TasksClient):
+    got = tasks_client.get_tasks()
+    assert got.status_code == 204
